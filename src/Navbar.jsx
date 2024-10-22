d816cd3bcb965bd81d7d2c3cb8d73eaf939c6152
Mery Álvarez <mery.alvarez.h@gmail.com>
2024-10-22 15:23:37 +0200

    WIP

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Navbar = () => {
+
+    console.log('hola')
+    return (
+        <div className="navbar bg-accent">
+            <a className="text-xl text-white">CrowdMap</a>
+        </div>
+    )
+}
