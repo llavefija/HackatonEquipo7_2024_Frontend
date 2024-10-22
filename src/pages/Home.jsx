89aac027cbe2a964e0b9a43d266480336310d41b
Valentina Toni <vale.toni93@gmail.com>
2024-10-22 16:13:08 +0200

    restablish commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import HeatMap from "../components/HeatMap";
+
+const Home = () => {
+  return <div>
+    <HeatMap />
+  </div>;
+};
+
+export default Home;
---
+++ src/router/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Layout from "../layout/Layout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ path: "", element: <Home /> }],
+  },
+]);
